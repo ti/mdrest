64017bf822d03d7371e81add7c71dd7dbcda88d7
李南希 <leenanxi@gmail.com>
2019-02-01 20:06:16 +0800

    add nodes to markdown

--- sitemap.go
+++ sitemap.go
@@ -1,21 +1,23 @@
 package mdrest
 
 import (
-	"strings"
+	"fmt"
 	"sort"
+	"strings"
 )
 
 type Node struct {
-	Title string `json:"title"`
+	Title    string `json:"title"`
 	Location string `json:"location,omitempty"`
-	Children Nodes `json:"children,omitempty"`
+	Icon     string `json:"icon,omitempty"`
+	Children Nodes  `json:"children,omitempty"`
 }
 
 type Nodes []Node
 
-func (n *Node) Add(path []string, title, location string){
+func (n *Node) Add(path []string, title, location, icon string) {
 	if len(path) == 0 {
-		n.Children = append(n.Children, Node{Title:title, Location:location})
+		n.Children = append(n.Children, Node{Title: title, Location: location, Icon:icon})
 		return
 	}
 	idx := -1
@@ -26,9 +28,9 @@ func (n *Node) Add(path []string, title, location string){
 	}
 	if idx == -1 {
 		idx = len(n.Children)
-		n.Children = append(n.Children, Node{Title:path[0]})
+		n.Children = append(n.Children, Node{Title: path[0]})
 	}
-	n.Children[idx].Add(path[1:], title, location)
+	n.Children[idx].Add(path[1:], title, location, icon)
 }
 
 func (n Nodes) Len() int {
@@ -53,13 +55,50 @@ func (this Articles) GetSiteMap(deep int) Nodes {
 	for _, article := range this {
 		a := (*article)
 		location := a[KeyLocation].(string)
-		sli := strings.SplitN(location, "/",deep + 1)
+		sli := strings.SplitN(location, "/", deep+1)
 		if len(sli) > deep {
 			continue
 		}
+		var icon string
+		if i, ok := a["icon"]; ok {
+			icon = fmt.Sprint(i)
+		}
 		folders := sli[:len(sli)-1]
-		siteMap.Add(folders,a[KeyTitle].(string),a[KeyLocation].(string))
+		siteMap.Add(folders, a[KeyTitle].(string), location, icon)
 	}
 	sort.Sort(siteMap.Children)
 	return siteMap.Children
+}
+
+
+
+var iconClass = "material-icons"
+// Markdown convert nodes to Markdown format
+func (n Nodes) ToMarkdown(intent ...string) (md string) {
+	if len(n) == 0 {
+		return ""
+	}
+	var iconClassTag string
+	if iconClass != "" {
+		iconClassTag = fmt.Sprintf(` class="%s"`, iconClass)
+	}
+
+	intents := strings.Join(intent, "")
+	for _, c := range n {
+		//TODO: remove
+		c.Location = strings.Replace(c.Location, " ", "%20", -1)
+		icon := c.Icon
+		if icon != "" {
+			icon = fmt.Sprintf(`<i%s>%s</i> `, iconClassTag, c.Icon)
+		}
+
+		line := fmt.Sprintf("%s* %s[%s](%s)\n", intents, icon, c.Title, c.Location)
+		md += line
+		if len(c.Children) > 0 {
+			newIntent := append(intent, "\t")
+			cmd := c.Children.ToMarkdown(newIntent...)
+			md += cmd
+		}
+	}
+	return
 }
